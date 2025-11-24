9c509d8a879684c5a46be03cd10f215dc07f7d49
Dan <blackravenn13@gmail.com>
2025-11-24 20:12:10 +0000

    feat: Configured as standard Farcaster Mini App

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  // --- KONFIGURASI BASE MINI APP BARU ---
-  baseBuilder: {
-    ownerAddress: "0x83327998662Af2174CFae423c31e5e2458257E34",
-  },
 };
 
 module.exports = nextConfig;

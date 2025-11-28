6dfe0a66bc33140b190d8136d686882a78c97e67
Dan <blackravenn13@gmail.com>
2025-11-28 04:47:35 +0000

    fix: Re-enabled edge runtime for /api/og to resolve dynamic server error

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,8 @@
 import { ImageResponse } from 'next/og';
 import { NextRequest } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);

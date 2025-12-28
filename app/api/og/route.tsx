01ce7b1bb91f74cfefd2ca0ff422e3a50b3ac593
Dan <blackravenn13@gmail.com>
2025-12-28 21:46:24 +0000

    update

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,8 @@
 import { ImageResponse } from 'next/og';
 import { NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export const runtime = 'edge';
 
 export async function GET(req: NextRequest) {
